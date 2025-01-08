32b81b7d48f1f05626185f611e2842f4e0b04e1b
SPRING26\mduna <mdunaji@gmail.com>
2025-01-07 19:44:28 -0500

    update

---
+++ Vector.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+list1 = [1, 2, 3, 4, 5]
+list2 = [5, 4, 3, 2, 1]
+
+v1 = np.array(list1)
+v2 = np.array(list2)
+
+print(v1)
+print(v2)
+addtion = v1 + v2
+print("v1+v2 =", addtion)
+subtraction = v1 - v2
+print(subtraction)
+multiplication = v1 * v2
+print(multiplication)
+division = v1 / v2
+print(division)
